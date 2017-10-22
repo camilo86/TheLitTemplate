a9d9507169bb28fb1508ab184ab50870c140b01c
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2017-10-22 00:07:52 -0400

    Added password hashing for accounts

--- models/account.js
+++ models/account.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var accountSchema = new mongoose.Schema({
   firstName: { type: mongoose.Schema.Types.String, required: true },
@@ -6,5 +7,26 @@ var accountSchema = new mongoose.Schema({
   email: { type: mongoose.Schema.Types.String, required: true },
   password: { type: mongoose.Schema.Types.String, required: true }
 });
+
+accountSchema.pre('save', function(next) {
+  var account = this;
+
+  if(!account.isModified('password')) {
+    next();
+  }
+
+  bcrypt.hash(account.password, function(error, hash) {
+    if(error) {
+      return next(error);
+    }
+
+    account.password = hash;
+    next();
+  });
+});
+
+accountSchema.methods.comparePassword = function(candidate, callback) {
+  bcrypt.compare(candidate, this.password, callback);
+}
 
 module.exports = mongoose.model('Account', accountSchema);
